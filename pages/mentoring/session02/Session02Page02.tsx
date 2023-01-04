0db3356bbcae456cba73baadbdd26e294af9b2be
NanaReyes <dianac.reyesmantilla@gmail.com>
2023-01-04 18:58:06 -0500

    Add the activities for the second session

---
+++ pages/mentoring/session02/Session02Page02.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Session02Page02: React.FC = () => {
+  return <>Session 02, Page 02</>;
+};
+
+export default Session02Page02;
---
+++ pages/mentoring/session02/Session02Page05.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Session02Page05: React.FC = () => {
+  return <>Session 02, Page 05</>;
+};
+
+export default Session02Page05;
