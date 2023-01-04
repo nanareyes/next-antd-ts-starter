dd80e2a54157e160851670e56926cf10f5752715
fredygil <fgil666@gmail.com>
2023-01-04 08:46:38 -0500

    first session

---
+++ pages/mentoring/session01/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Alert } from "antd";
+
+const Session01Page: React.FC = () => {
+  return <Alert message="Hi, this is session 01" type="success" />;
+};
+
+export default Session01Page;
