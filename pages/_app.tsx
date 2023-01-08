e95c2d46dac4578ab169055deccc138c71b88948
fredygil <fgil666@gmail.com>
2023-01-07 20:53:47 -0500

    upgrade nextjs to v13 and antd to v5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "antd/dist/antd.css";
+import "antd/dist/reset.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
