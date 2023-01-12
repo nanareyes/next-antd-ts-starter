58f9024019c7eb44cd7387f1a8e5e9a8c9c06aca
NanaReyes <dianac.reyesmantilla@gmail.com>
2023-01-12 15:23:45 -0500

    Add activity 4 to change the colors in the component and add another card at the beginning or end of the card

--- pages/mentoring/session04/index.tsx
+++ pages/mentoring/session04/index.tsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import randomColor from "randomcolor";
 import ColorBoxes from "./ColorBoxes";
+import { Button } from "antd";
 
 const Session03Page: React.FC = () => {
   const [colors, setColors] = useState<string[]>([]);
+  // const [numColors, setNumColors] = useState<number>(5);
 
   useEffect(() => {
     setColors(generateRandomColors());
   }, []);
 
-  const generateRandomColors = () => {
-    return Array(5)
+  //const numColors = 5;
+  const generateRandomColors = (numColors = 5) => {
+    return Array(numColors)
       .fill(null)
       .map((_) => randomColor());
   };
 
+  const addNewColor = () => {
+    //numColors + 1;
+    setColors([randomColor(), ...colors]);
+    //setNumColors(numColors + 1);
+  };
   return (
     <>
       {/**
@@ -22,6 +30,27 @@ const Session03Page: React.FC = () => {
        * TODO: Adicionar un botón con el label "New color". Al darle clic se debe crear una nueva caja de color al final de las existentes
        *
        */}
+      <Button
+        type="primary"
+        size={"middle"}
+        onClick={() => {
+          setColors(generateRandomColors(colors.length));
+        }}
+      >
+        Update colors
+      </Button>
+      <Button type="primary" size={"middle"} onClick={addNewColor}>
+        Add new color start
+      </Button>
+      <Button
+        type="primary"
+        size={"middle"}
+        onClick={() => {
+          setColors([...colors, randomColor()]);
+        }}
+      >
+        Add new color end
+      </Button>
       <ColorBoxes colors={colors} />
     </>
   );
